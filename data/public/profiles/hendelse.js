6ded6722d2ad4832ea3401b879bcac27cd457e9d
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2014-06-10 04:49:41 +0200

    add profile: hendelse

---
+++ data/public/profiles/hendelse.js
@@ -0,0 +1,106 @@
+var profile = {
+  "overview": {
+    "title": "Hendelse",
+    "desc": "Beskriver en historisk hendelse",
+    "type": "<http://purl.org/NET/c4dm/event.owl#Event>"
+  },
+  "views": [
+    {
+      "title": "Beskrivelse",
+      "desc": "",
+      "elements": [
+        {
+          "id": "title",
+          "label": "Navn",
+          "desc": "",
+          "required": true,
+          "repeatable": false,
+          "predicates": [
+            {
+              "label": "navn",
+              "uri": "<http://purl.org/dc/terms/title>"
+            }
+          ],
+          "type": "langString"
+        },
+        {
+          "id": "desc",
+          "label": "Beskrivelse",
+          "desc": "Hva skjedde egentlig",
+          "required": false,
+          "repeatable": false,
+          "predicates": [
+            {
+              "label": "beskrivelse",
+              "uri": "<http://purl.org/dc/terms/description>"
+            }
+          ],
+          "type": "langString"
+        },
+        {
+          "id": "place",
+          "label": "Sted",
+          "desc": "Hvor skjedde det?",
+          "required": false,
+          "repeatable": true,
+          "predicates": [
+            {
+              "label": "sted",
+              "uri": "<http://purl.org/NET/c4dm/event.owl#place>"
+            }
+          ],
+          "type": "URI",
+          "searchTypes": ["sted"]
+        },
+        {
+          "id": "time",
+          "label": "Tid",
+          "desc": "Når skjedde det?",
+          "required": false,
+          "repeatable": true,
+          "predicates": [
+            {
+              "label": "tid",
+              "uri": "<http://purl.org/NET/c4dm/event.owl#time>"
+            }
+          ],
+          "type": "URI",
+          "searchTypes": ["temporalEntitet"]
+        },
+        {
+          "id": "agent",
+          "label": "Aktør",
+          "desc": "Hvem gjorde det?",
+          "required": false,
+          "repeatable": true,
+          "predicates": [
+            {
+              "label": "aktør",
+              "uri": "<http://purl.org/NET/c4dm/event.owl#agent>"
+            }
+          ],
+          "type": "URI",
+          "searchTypes": ["agent"]
+        }
+      ]
+    }
+  ],
+  "displayLabel": function( values ) {
+    var label = "";
+    if ( values.title[0] ) {
+      label = cleanString( values.title[0].value );
+    }
+    return '"' + label + '"';
+  },
+  "searchLabel": function( values ) {
+    var label = "";
+    if ( values.title[0] ) {
+      label = cleanString( values.title[0].value );
+    }
+    return '"' + label + '"';
+  },
+  "rules": [
+    "SPARQL ditt",
+    "SPARQL datt"
+  ]
+};
