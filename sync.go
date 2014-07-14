6a880c801d4584442f5521275ee83ae80fbd5f77
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2014-07-14 10:12:50 +0200

    syncUpdatedManifestation WIP

--- sync.go
+++ sync.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"bytes"
+	"encoding/xml"
 	"errors"
 	"fmt"
 	"net/http"
 	"net/http/cookiejar"
 )
+
+var svcNotAuthorized = errors.New("unauthorized")
+
+// svcResponse represents the XML response we get from the svc API when updating
+// or creating a biblio.
+type svcResponse struct {
+	XMLName xml.Name `xml:"response"`
+	Biblio  int      `xml:"biblionumber"`
+	Status  string   `xml:"status"`
+	Error   string   `xml:"error"`
+	// We are not interested in the marcxml record,
+	// so we ignore the rest of the response.
+}
 
 // syncKohaAuth attemps to authenticate against Koha's /svc endpoint.
 // If sucessfull, it returns a cookiejar with the authenticated cookie.
@@ -26,24 +41,60 @@ func syncKohaAuth(kohaPath, user, pass string) (http.CookieJar, error) {
 	return client.Jar, nil
 }
 
-// 3 kinds of sync: create,update,delete
-
 // 1. Manifestation created
 // -> POST /svc/new_bib
 //    hold on to biblionumber and store in RDF:
 //    -> insert { <manifestation> armillaria:kohaId biblionumber }
-func syncUpdatedManifestation(rec *marcRecord, uri string) error {
-	return nil
+func syncNewManifestation(kohaPath string, jar http.CookieJar, rec marcRecord) (int, error) {
+	return 122345, nil
 }
 
 // 2. Manifestation updated
 // -> POST /svc/{biblionumber}
-func syncNewManifestation(rec *marcRecord, uri string) (string, error) {
-	return "biblionumber", nil
+func syncUpdatedManifestation(kohaPath string, jar http.CookieJar, marc []byte, biblio int) error {
+	path := fmt.Sprintf("%s/cgi-bin/koha/svc/bib/%d", kohaPath, biblio)
+	client := &http.Client{Jar: jar}
+	req, err := http.NewRequest("GET", path, bytes.NewReader(marc))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "text/xml")
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		switch resp.StatusCode {
+		case 403:
+			return svcNotAuthorized
+		case 404:
+			return errors.New("not found")
+		case 400:
+			return errors.New("bad request")
+		default:
+			// Should never be reached.
+			// There are no other errors, according to the svc source code.
+			return errors.New("unknown error")
+		}
+	}
+
+	var svcRes svcResponse
+	d := xml.NewDecoder(resp.Body)
+
+	if err := d.Decode(&svcRes); err != nil {
+		return err
+	}
+	if svcRes.Status != "ok" {
+		return errors.New(svcRes.Error)
+	}
+
+	return nil
 }
 
 // 3. Manifestation deleted
 // -> DELETE /svc/{biblionumber}
-func syncDeletedManifestation(biblio string) error {
+func syncDeletedManifestation(kohaPath string, jar http.CookieJar, biblio string) error {
 	return errors.New("svc API does not support deletion yet")
 }
--- sync_test.go
+++ sync_test.go
@@ -7,7 +7,57 @@ import (
 	"testing"
 )
 
-var authServer *httptest.Server
+var (
+	authServer      *httptest.Server
+	svcUpdateServer *httptest.Server
+)
+
+const updateResponse = `<?xml version='1.0' standalone='yes'?>
+<response>
+  <biblionumber>164442</biblionumber>
+  <marcxml>
+<record xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd" xmlns="http://www.loc.gov/MARC21/slim">
+  <leader>00679cam a22002291  4500</leader>
+  <controlfield tag="000">c</controlfield>
+  <controlfield tag="008">                      a          00nob  </controlfield>
+  <datafield tag="015" ind1=" " ind2=" ">
+    <subfield code="a">241</subfield>
+    <subfield code="b">BibliofilID</subfield>
+  </datafield>
+  <datafield tag="019" ind1=" " ind2=" ">
+    <subfield code="b">l</subfield>
+  </datafield>
+  <datafield tag="020" ind1=" " ind2=" ">
+    <subfield code="a">8205183546</subfield>
+    <subfield code="c">Nkr199,99.</subfield>
+  </datafield>
+  <datafield tag="100" ind1=" " ind2="0">
+    <subfield code="a">Brox, Ottar</subfield>
+    <subfield code="d">1932-</subfield>
+    <subfield code="j">n.</subfield>
+    <subfield code="3">10033300</subfield>
+  </datafield>
+  <datafield tag="245" ind1="1" ind2="0">
+    <subfield code="a">Bygdenæringene kan saktens bli lønnsomme</subfield>
+    <subfield code="c">Ottar Brox</subfield>
+  </datafield>
+  <datafield tag="260" ind1=" " ind2=" ">
+    <subfield code="a">Oslo</subfield>
+    <subfield code="b">Gyldendal</subfield>
+    <subfield code="c">1989</subfield>
+  </datafield>
+  <datafield tag="300" ind1=" " ind2=" ">
+    <subfield code="a">187 s.</subfield>
+    <subfield code="b">fig.</subfield>
+  </datafield>
+  <datafield tag="999" ind1=" " ind2=" ">
+    <subfield code="c">164442</subfield>
+    <subfield code="d">164442</subfield>
+  </datafield>
+</record>
+</marcxml>
+  <status>ok</status>
+</response>`
 
 func init() {
 	authServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +65,7 @@ func init() {
 		pass := r.URL.Query().Get("password")
 
 		if user != "sync" || pass != "sync" {
-			http.Error(w, "missing required parameter: query", http.StatusForbidden)
+			http.Error(w, "wrong userid or password", http.StatusForbidden)
 			return
 		}
 
@@ -23,6 +73,15 @@ func init() {
 			MaxAge: 0, Name: "CGISESSID", Value: "8655024ef41e104a1a2c58a6c744e69c"}
 		http.SetCookie(w, &cookie)
 
+	}))
+
+	svcUpdateServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("CGISESSID")
+		if err == nil && cookie.Value == "8655024ef41e104a1a2c58a6c744e69c" {
+			w.Write([]byte(updateResponse))
+			return
+		}
+		http.Error(w, "unathorized", http.StatusForbidden)
 	}))
 }
 
@@ -34,7 +93,7 @@ func TestSyncKohaAuth(t *testing.T) {
 
 	jar, err := syncKohaAuth(authServer.URL, "sync", "sync")
 	if err != nil {
-		t.Error("syncKohaAuth with correct user & pass result in error: %v", err)
+		t.Fatal("syncKohaAuth with correct user & pass result in error: %v", err)
 	}
 	u, err := url.Parse(authServer.URL)
 	if err != nil {
@@ -52,9 +111,14 @@ func TestSyncKohaAuth(t *testing.T) {
 }
 
 func TestUpdatedManifestation(t *testing.T) {
-	cfg = &config{
-		KohaPath:     "localhost:1234",
-		KohaSyncUser: "sync",
-		KohaSyncPass: "sync",
+	jar, err := syncKohaAuth(authServer.URL, "sync", "sync")
+	if err != nil {
+		t.Fatal(err)
 	}
+
+	err = syncUpdatedManifestation(svcUpdateServer.URL, jar, []byte("<marcxml> ... </marcxml>"), 164442)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
 }
